983f4987c2a93c5b729629ccb4b9620f591de370
Dmitry Rog <dmitry.rog.jrn@gmail.com>
2021-08-07 20:49:27 +0000

    Add return statements

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -18,10 +18,12 @@ func Article(w http.ResponseWriter, r *http.Request) {
 	url := queryValues.Get("q")
 	if url == "" {
 		http.Error(w, "Must specify the url to request", 400)
+		return
 	}
 	response, err := http.Get(url)
 	if err != nil {
 		http.Error(w, "Failed to retrieve article", 500)
+		return
 	}
 	if response.StatusCode >= 400 {
 		if err != nil {
